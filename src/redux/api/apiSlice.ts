0674a4a61e2b521610d029fb2a62dfa5321a4d76
sumoncse19 <mdsumoncse19@gmail.com>
2024-06-06 01:17:50 +0600

    update backend link

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://fluent-job-hub-backend.onrender.com",
+  }),
   tagTypes: ["companies", "employees", "reviews"],
   endpoints: () => ({}),
 });
